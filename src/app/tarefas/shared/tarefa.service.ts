05039594c440c8fb96cc6f479b0fff429d090a36
Kevin Ferreira <ferreirakevin325@gmail.com>
2021-12-12 09:02:14 -0300

    criação do CRUD no service

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -7,4 +7,46 @@ import { Tarefa } from './tarefa.model';
 export class TarefaService {
 
   constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    return tarefas.find(Tarefa => Tarefa.id === id);
+  }
+
+  atualizar(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        obj[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].concluida = !obj.concluida;
+      }
+    })
+  }
 }
