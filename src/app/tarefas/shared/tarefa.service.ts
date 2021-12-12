748fc0a12ecf7158c6647e193897a4582369dd5c
Kevin Ferreira <ferreirakevin325@gmail.com>
2021-12-12 07:54:09 -0300

    Ajustes e imports

---
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Tarefa } from './tarefa.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarefaService {
+
+  constructor() { }
+}
