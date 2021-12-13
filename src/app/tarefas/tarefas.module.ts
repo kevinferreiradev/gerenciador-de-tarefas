e2dbcb56249caa6883b8d5c62fa4dfa9d836cf3f
Kevin Ferreira <ferreirakevin325@gmail.com>
2021-12-13 00:47:55 -0300

    criacao componente editar e ajustes

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarComponent } from './editar';
 
 
 
 @NgModule({
   declarations: [
     ListarTarefaComponent,
-    CadastrarTarefaComponent
+    CadastrarTarefaComponent,
+    EditarComponent
   ],
   imports: [
     CommonModule,
