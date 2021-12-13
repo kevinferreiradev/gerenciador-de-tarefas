ff2fdbf37056dfd26ea24dfdf8d2738284f8760d
Kevin Ferreira <ferreirakevin325@gmail.com>
2021-12-13 02:15:29 -0300

    criação da diretiva concluido e finalização do projeto

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
 import { EditarComponent } from './editar';
@@ -14,7 +14,8 @@ import { EditarComponent } from './editar';
   declarations: [
     ListarTarefaComponent,
     CadastrarTarefaComponent,
-    EditarComponent
+    EditarComponent,
+    TarefaConcluidaDirective
   ],
   imports: [
     CommonModule,
